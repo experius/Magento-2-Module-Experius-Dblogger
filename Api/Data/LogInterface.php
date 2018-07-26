1f9829f25edc02e1f3ba912d5a7007dace842841
Casper Frericks <casper.frericks@experius.nl>
2018-07-26 10:41:37 +0000

    [REFACTOR] - Refactored code indentation, tabs to spaces, Windows to Unix line endings, new line at end of file, PHPDoc, type setting.

--- Api/Data/LogInterface.php
+++ Api/Data/LogInterface.php
@@ -2,19 +2,19 @@
 /**
  * Database logger
  * Copyright (C) 2017  Experius
- * 
+ *
  * This file is part of Experius/Dblogger.
- * 
+ *
  * Experius/Dblogger is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -23,20 +23,40 @@ namespace Experius\Dblogger\Api\Data;
 
 interface LogInterface
 {
-
+    /**
+     * @var string
+     */
     const MODULE = 'module';
-    const ACTION = 'action';
-    const CREATED_AT = 'created_at';
-    const LOG_ID = 'log_id';
-	const TYPE = 'type';
-    const MESSAGE = 'message';
 
+    /**
+     * @var string
+     */
+    const ACTION = 'action';
+
+    /**
+     * @var string
+     */
+    const CREATED_AT = 'created_at';
+
+    /**
+     * @var string
+     */
+    const LOG_ID = 'log_id';
+
+    /**
+     * @var string
+     */
+    const TYPE = 'type';
+
+    /**
+     * @var string
+     */
+    const MESSAGE = 'message';
 
     /**
      * Get log_id
      * @return string|null
      */
-    
     public function getLogId();
 
     /**
@@ -44,14 +64,12 @@ interface LogInterface
      * @param string $log_id
      * @return Experius\Dblogger\Api\Data\LogInterface
      */
-    
     public function setLogId($logId);
 
     /**
      * Get module
      * @return string|null
      */
-    
     public function getModule();
 
     /**
@@ -59,14 +77,12 @@ interface LogInterface
      * @param string $module
      * @return Experius\Dblogger\Api\Data\LogInterface
      */
-    
     public function setModule($module);
 
     /**
      * Get action
      * @return string|null
      */
-    
     public function getAction();
 
     /**
@@ -74,14 +90,12 @@ interface LogInterface
      * @param string $action
      * @return Experius\Dblogger\Api\Data\LogInterface
      */
-    
     public function setAction($action);
 
     /**
      * Get message
      * @return string|null
      */
-    
     public function getMessage();
 
     /**
@@ -89,14 +103,12 @@ interface LogInterface
      * @param string $message
      * @return Experius\Dblogger\Api\Data\LogInterface
      */
-    
     public function setMessage($message);
-	
-	/**
+
+    /**
      * Get type
      * @return string|null
      */
-    
     public function getType();
 
     /**
@@ -104,14 +116,12 @@ interface LogInterface
      * @param string $type
      * @return Experius\Dblogger\Api\Data\LogInterface
      */
-    
     public function setType($type);
 
     /**
      * Get created_at
      * @return string|null
      */
-    
     public function getCreatedAt();
 
     /**
@@ -119,6 +129,5 @@ interface LogInterface
      * @param string $created_at
      * @return Experius\Dblogger\Api\Data\LogInterface
      */
-    
     public function setCreatedAt($created_at);
 }
--- Controller/Adminhtml/Log/Index.php
+++ Controller/Adminhtml/Log/Index.php
@@ -2,19 +2,19 @@
 /**
  * Database logger
  * Copyright (C) 2017  Experius
- * 
+ *
  * This file is part of Experius/Dblogger.
- * 
+ *
  * Experius/Dblogger is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -23,7 +23,9 @@ namespace Experius\Dblogger\Controller\Adminhtml\Log;
 
 class Index extends \Magento\Backend\App\Action
 {
-
+    /**
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
     protected $resultPageFactory;
 
     /**
@@ -48,7 +50,7 @@ class Index extends \Magento\Backend\App\Action
     public function execute()
     {
         $resultPage = $this->resultPageFactory->create();
-            $resultPage->getConfig()->getTitle()->prepend(__("Log"));
-            return $resultPage;
+        $resultPage->getConfig()->getTitle()->prepend(__("Log"));
+        return $resultPage;
     }
 }
